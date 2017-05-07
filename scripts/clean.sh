60e45285f242b7a079c69ec0bc576a6485cc3299
Mary Alexis Solis <mary@noteabout.com>
2017-05-07 18:12:51 +0800

    - Added aes folder

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,6 @@
-rm ./results/aes/*
-rm ./results/rsa/*
-rm ./results/sha/*
+rm -rf -d results/aes
+rm -rf -d results/rsa
+rm -rf -d results/sha
+mkdir results/aes
+mkdir results/rsa
+mkdir results/sha
