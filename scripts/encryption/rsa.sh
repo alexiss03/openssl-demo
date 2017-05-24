19449d82c8142d152e3f8cbd5da2a5a9fb5accc6
Mary Alexis Solis <mary@noteabout.com>
2017-05-24 21:18:04 +0800

    - Replaced keys for RSA encryption - Added OpenSSLDemo.pdf

--- scripts/encryption/rsa.sh
+++ scripts/encryption/rsa.sh
@@ -1,3 +1,3 @@
-echo resources/lena512color.tiff | openssl rsautl -encrypt -pubin -inkey resources/keys/rsa2048.pub.pem  > results/rsa/lena512-rsa2048.dat
+echo resources/lena512color.tiff | openssl rsautl -encrypt -pubin -inkey resources/keys/rsa2048-public.pem  > results/rsa/lena512-rsa2048.dat
 
 openssl dgst -sha256 -sign resources/keys/ecdsa-private.pem -out results/rsa/lena512-ecdsa-sha256.dat resources/lena512color.tiff
